bdfccb0c2cd2e8c606e7d321c8b94c9a2063c826
conblem <mail@conblem.me>
2019-02-22 07:07:29 +0100

    Added styled components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { hot } from "react-hot-loader/root";
 import { render } from "react-dom";
 import React from "react";
 
-const App = () => <h1>Test 3213</h1>;
-const Entry = process.env.NODE_ENV === "development" ? hot(App) : App;
-render(<Entry />, document.body);
+import { App } from "./container/App";
+
+render(<App />, document.body);
